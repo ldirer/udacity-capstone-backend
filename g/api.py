03b1b4736c4884ff1199e0cf13933407a7e60c13
Laurent Direr <laurent.direr@gmail.com>
2016-10-14 12:44:55 +0200

    Minor changes for the lazy Android coder

--- g/api.py
+++ g/api.py
@@ -1,8 +1,8 @@
 import json
 from datetime import timezone, datetime
 from flask import request
-from flask.ext.restless.helpers import url_for
-from flask.ext.restless.manager import APIManager
+from flask_restless.helpers import url_for
+from flask_restless.manager import APIManager
 from flask.wrappers import Response
 from pytz import UTC
 
@@ -11,11 +11,19 @@ from g.models import Language, User, Attempt, UserLanguage
 
 from g.translator import main_script
 
+"""I tried flask_restless 1.xx (beta). I don't like that the response has so many nested levels (like a top-level "attributes" for the data we usually want!...
+So I reverted in spite of some nice new features, like the ability to PATCH relationships directly on an object.
+e.g. I can specify I want to add languages to a user by PATCHing on /api/user/5/languages.
+"""
+
 api_manager = APIManager(app, flask_sqlalchemy_db=db)
-api_manager.create_api(Language, methods=['GET'], max_results_per_page=-1)
-api_manager.create_api(User, methods=['GET', 'POST'])
-api_manager.create_api(Attempt, methods=['GET', 'POST'])
-api_manager.create_api(UserLanguage, methods=['GET', 'POST'])
+api_manager.create_api(Language, methods=['GET'])
+# flask_restless v0.17 (no mo' include_methods afterwards)
+api_manager.create_api(User, methods=['GET', 'POST'], include_methods=['attempts.language_id'])
+
+api_manager.create_api(User, methods=['GET', 'POST', 'PUT', 'PATCH'])
+api_manager.create_api(Attempt, methods=['GET', 'POST'], include_methods=['language_id'])
+api_manager.create_api(UserLanguage, methods=['GET', 'POST'], include_methods=['language_name'])
 
 
 @app.route("/")
@@ -31,22 +39,25 @@ def translate():
 
 
 def get_last_update(user_id):
-    user_language_ts = UserLanguage.query.filter_by(user_id=user_id).order_by(
-        "created_timestamp").first().created_timestamp
-    if user_language_ts is None:
+    """Get the timestamp for the latest data we have about a user. 
+    0 if there's no action.
+    """
+    user_language = UserLanguage.query.filter_by(user_id=user_id).order_by(UserLanguage.created_timestamp.desc()).first()
+    if user_language is None:
         return 0
 
-    attempt_ts = Attempt.query.filter_by(user_id=user_id).order_by("timestamp").first().timestamp
-    if attempt_ts is None:
-        return user_language_ts
+    user_language_ts = user_language.created_timestamp.replace(tzinfo=UTC).timestamp()
 
-    return max(user_language_ts.replace(tzinfo=UTC).timestamp(),
-               attempt_ts.replace(tzinfo=UTC).timestamp())
+    attempt = Attempt.query.filter_by(user_id=user_id).order_by(Attempt.timestamp.desc()).first()
+    attempt_ts = 0 if attempt is None else attempt.timestamp.replace(tzinfo=UTC).timestamp()
+
+    return max(user_language_ts, attempt_ts)
 
 
 CLIENT_TO_SERVER_SYNC = 'sync_to_server'
 SERVER_TO_CLIENT_SYNC = 'sync_to_client'
 CLIENT_TO_SERVER_USER_SYNC = 'sync_user_to_server'
+ALREADY_IN_SYNC = 'already_in_sync'
 
 
 @app.route("/polling")
@@ -60,13 +71,18 @@ def polling():
     # api_manager.created_apis_for['language']
     if not existing_user:
         # We want the new user and everything the client has got.
-        return json.dumps({'action': CLIENT_TO_SERVER_USER_SYNC, 'urls': [url_for(User)]})
+        return json.dumps({'action': CLIENT_TO_SERVER_USER_SYNC, 'urls': {"user": url_for(User)}})
 
     last_update_db = get_last_update(existing_user._id)
-    if not existing_user or last_update_client > last_update_db:
+    if last_update_client > last_update_db:
         # We want the client to give us everything they got since our last update.
-        return json.dumps({'action': CLIENT_TO_SERVER_SYNC, 'last_update_unix': last_update_db})
-    else:
+        # We send it the url so it can send PATCH requests directly on the right instance.
+        return json.dumps({
+            'action': CLIENT_TO_SERVER_SYNC, 
+            'url': url_for(User, instid=existing_user._id),
+            'last_update_unix': last_update_db
+            })
+    elif last_update_db > last_update_client:
         # We need to send updates to the client
 
         # We should probably be packing all the changes here and send them directly.
@@ -84,4 +100,10 @@ def polling():
             '{"name": "user_id", "op": "==", "val": %i}]}'
             % (datetime.fromtimestamp(last_update_client, tz=UTC), existing_user._id))
 
-        return json.dumps({'action': SERVER_TO_CLIENT_SYNC, 'urls': [attempts_update_url, user_language_update_url]})
+        # We could imagine restricting the urls to those that actually have a fresher timestamp.
+        return json.dumps({'action': SERVER_TO_CLIENT_SYNC, 
+            'urls': {"attempt": attempts_update_url, "user_language": user_language_update_url}})
+    else:
+        # We have the same timestamp for last update: we're in sync.
+        return json.dumps({'action': ALREADY_IN_SYNC, 'urls': {}})
+
--- g/models.py
+++ g/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.ext.associationproxy import association_proxy
 
 from g import db
 
@@ -15,8 +16,10 @@ class UserLanguage(db.Model):
     user_id = db.Column(db.Integer, ForeignKey("user._id"), nullable=False)
     language_id = db.Column(db.Integer, ForeignKey("language._id"), nullable=False)
     created_timestamp = db.Column(db.DateTime(), index=True)
+    language = db.relationship(Language)
 
-
+    def language_name(self):
+        return self.language.name
 
 
 class Word(db.Model):
@@ -28,11 +31,26 @@ class Word(db.Model):
 
 
 class Attempt(db.Model):
+
     _id = db.Column(db.Integer, primary_key=True)
     word_id = db.Column(db.Integer, ForeignKey("word._id"))
+    word = db.relationship(Word)
+    # This is lame. I want this key in my JSON (it's nice to have it in the sqlite db to avoid an extra join).
+    # However I dont know how to include it as a 'computed attribute on the Word relationship'. So quick and dirty. And unsecure.
+    # language_id = db.Column(db.Integer)
+
+    # Here's the right solution! unfortunately it does not appear in flask-restless response anymore... I'm hacking this with a selector method and the `include_methods` parameter on create_api.
+    # language_id = association_proxy('word', 'language_id')
     user_id = db.Column(db.Integer, ForeignKey("user._id"))
     success = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime)
+
+    def language_id(self):
+        """This simple method and the `include_methods` parameter on `create_api` looks like the best solution."""
+        # Initially I was doing: return self.language_id
+        # Actually we should do self.word.language_id. The association proxy is not useful here, it would be if we wanted to update the word language id directly from the attempt.
+        # Leaving it commented cause association proxies are cool.
+        return self.word.language_id
 
 
 class User(db.Model):
